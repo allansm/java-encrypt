40f7785f4a059eaa0792d05c35be812401a98b52
Allan De Souza Melo <allan_de_souza_melo@hotmail.com>
2020-03-12 02:40:46 -0300

    update

--- src/persistence/DataDao.java
+++ src/persistence/DataDao.java
@@ -23,6 +23,19 @@ public class DataDao {
 		}
 		Files.write(Paths.get(filename+"."+data.getKey()),data.getEncrypted().getBytes());
 	}
+	public void write(Data data,String filename,Encrypter.TYPE type,Alphabet alphabet) throws Exception{
+		Encrypter encrypter = new Encrypter();
+		if(type.equals(Encrypter.TYPE.FASTRANDOM)) {
+			data = encrypter.fastRandomEncrypt(data, alphabet);
+		}else if(type.equals(Encrypter.TYPE.FASTLINEAR)) {
+			data = encrypter.fastLinearEncrypt(data, alphabet);
+		}else if(type.equals(Encrypter.TYPE.RANDOM)) {
+			data = encrypter.randomEncrypt(data, alphabet);
+		}else if(type.equals(Encrypter.TYPE.LINEAR)) {
+			data = encrypter.linearEncrypt(data, alphabet);
+		}
+		Files.write(Paths.get(filename),data.getEncrypted().getBytes());
+	}
 	public Data read(String path,Alphabet alphabet) throws Exception{
 		byte[] b = Files.readAllBytes(Paths.get(path));
 		Data data = new Data();
--- src/view/Console.java
+++ src/view/Console.java
@@ -12,44 +12,93 @@ import entity.Encrypter;
 import persistence.DataDao;
 
 public class Console {
-	public void run() throws Exception{
-		Scanner scanner = new Scanner(System.in);
-		while(true) {
-			System.out.print("type file name:");
-			String fn = scanner.next();
-			System.out.print("url of file to encrypt:");
-			String url = scanner.next();
-			Data data = new Data();
-			data.setDecrypted(Files.readAllBytes(Paths.get(url)));
-			DataDao dataDao = new DataDao();
-			dataDao.write(data, fn,Encrypter.TYPE.FASTRANDOM);
-			System.out.println("\nok");
-			break;
-		}
+	private String inputFileUrl;
+	private String outputFileUrl;
+	private String customKey;
+	private int key;
+	
+	public void setInputFileUrl(Scanner scanner) {
+		System.out.print("type the input file url:");
+		this.inputFileUrl = scanner.next();
 	}
-	public void test()throws Exception {
-		byte[] b = Files.readAllBytes(Paths.get("test.20"));
-		System.out.println(new String(b));
+	public void setOutputFileUrl(Scanner scanner) {
+		System.out.print("type the output file url:");
+		this.outputFileUrl = scanner.next();
+	}
+	public void setKey(Scanner scanner) {
+		System.out.print("type key number:");
+		this.key = scanner.nextInt();
+	}
+	public void setCustomKey() throws Exception {
+		byte[] b = Files.readAllBytes(Paths.get(inputFileUrl));
 		Data data = new Data();
 		data.setEncrypted(new String(b));
-		data = new Encrypter().decrypt(data, new Alphabet(20));
-		Files.write(Paths.get("testa"), data.getDecrypted());
+		this.customKey = new String(new Encrypter().decrypt(data, new Alphabet(key)).getDecrypted());
 	}
-	public void test2() {
-		List<String> lista = new ArrayList<>();
-		for(int i=0;i<3;i++) {
-			lista.add(i, "");
-		}
-		lista.add(2, "2");
-		lista.add(1, "1");
-		lista.add(0, "0");
-		for(String s:lista) {
-			System.out.println(s);
+	public void encryptFile()throws Exception {
+		Data data = new Data();
+		data.setDecrypted(Files.readAllBytes(Paths.get(inputFileUrl)));
+		DataDao dataDao = new DataDao();
+		dataDao.write(data, outputFileUrl,Encrypter.TYPE.FASTRANDOM);
+		System.out.println("\nok");
+	}
+	public void decryptFile() throws Exception{
+		byte[] b = Files.readAllBytes(Paths.get(inputFileUrl));
+		Data data = new Data();
+		data.setEncrypted(new String(b));
+		data = new Encrypter().decrypt(data, new Alphabet(key));
+		Files.write(Paths.get(outputFileUrl), data.getDecrypted());
+	}
+	public void encryptUsingCustomKey() throws Exception{
+		Data data = new Data();
+		data.setDecrypted(Files.readAllBytes(Paths.get(inputFileUrl)));
+		DataDao dataDao = new DataDao();
+		dataDao.write(data, outputFileUrl,Encrypter.TYPE.FASTRANDOM,new Alphabet(customKey));
+		System.out.println("\nok");
+	}
+	public void decryptUsingCustomKey() throws Exception{
+		byte[] b = Files.readAllBytes(Paths.get(inputFileUrl));
+		Data data = new Data();
+		data.setEncrypted(new String(b));
+		data = new Encrypter().decrypt(data, new Alphabet(customKey));
+		Files.write(Paths.get(outputFileUrl), data.getDecrypted());
+	}
+	public void showVars() {
+		System.out.println("inputFileUrl:"+inputFileUrl);
+		System.out.println("outputFileUrl:"+outputFileUrl);
+		System.out.println("key:"+key);
+		System.out.println("customKey:"+customKey);
+	}
+	public void run() throws Exception{
+		Scanner scanner;
+		while(true) {
+			System.out.print("10110101>");
+			scanner = new Scanner(System.in);
+			String op  = scanner.next();
+			if(op.equals("setInputFileUrl")) {
+				setInputFileUrl(scanner);
+			}else if(op.equals("setOutputFileUrl")) {
+				setOutputFileUrl(scanner);
+			}else if(op.equals("setKey")) {
+				setKey(scanner);
+			}else if(op.equals("setCustomKey")) {
+				setCustomKey();
+			}else if(op.equals("encryptFile"))  {
+				encryptFile();
+			}else if(op.equals("decryptFile"))  {
+				decryptFile();
+			}else if(op.equals("encryptUsingCustomKey"))  {
+				encryptUsingCustomKey();
+			}else if(op.equals("decryptUsingCustomKey")) {
+				decryptUsingCustomKey();
+			}else if(op.equals("showVars")) {
+				showVars();
+			}
 		}
 	}
 	public static void main(String[] args) {
 		try {
-			//new Console().run();
+			new Console().run();
 			//new Console().test();
 			//new Console().test2();
 		}catch(Exception e) {
